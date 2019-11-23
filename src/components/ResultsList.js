4c020543e21333a27dbee37ce3d111c83e11251f
Kyle Phillips <khillips375@yahoo.com>
2019-11-23 18:39:11 -0500

    Styleing and spacing

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -3,7 +3,7 @@ import { View,Text,StyleSheet, FlatList} from 'react-native';
 import ResultsDetail from '../components/ResultDetail'
 const ResultsList = ({title, results}) => {
     return (
-        <View>
+        <View style={styles.container}>
             <Text style={styles.titleStyle}>{title}</Text>
             <FlatList 
             horizontal
@@ -20,8 +20,13 @@ const ResultsList = ({title, results}) => {
 }
 const styles = StyleSheet.create({
     titleStyle: {
+        marginLeft:15,
         fontSize:20,
-        fontWeight:'bold'
+        fontWeight:'bold',
+        marginBottom:5
+    },
+    container: {
+        marginBottom:10
     }
 })
 
