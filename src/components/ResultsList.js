e9a3a8ef3ca3c1f7fb3ed3e58e6254e2dba11bc1
Kyle Phillips <khillips375@yahoo.com>
2019-11-26 16:36:45 -0500

    Touchable Opacity navigates to Next Screen

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,19 +1,27 @@
 import React from 'react'
-import { View,Text,StyleSheet, FlatList} from 'react-native';
-import ResultsDetail from '../components/ResultDetail'
-const ResultsList = ({title, results}) => {
+import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
+import ResultsDetail from '../components/ResultDetail';
+
+
+const ResultsList = ({ title, results, navigation }) => {
     return (
         <View style={styles.container}>
             <Text style={styles.titleStyle}>{title}</Text>
-            <FlatList 
-            horizontal
-            // Removes Scroll bar 
-            showsHorizontalScrollIndicator={false}
-            data={results}
-            keyExtractor={(result) => result.id}
-            renderItem={({item}) => {
-                return <ResultsDetail result={item} />
-            }}
+            <FlatList
+                horizontal
+                // Removes Scroll bar 
+                showsHorizontalScrollIndicator={false}
+                data={results}
+                keyExtractor={(result) => result.id}
+                renderItem={({ item }) => {
+
+                    return (
+                        <TouchableOpacity onPress={() => navigation.navigate('ResultsShow')}>
+                            <ResultsDetail result={item} />
+                        </TouchableOpacity>
+                    )
+
+                }}
 
             />
         </View>
@@ -22,13 +30,13 @@ const ResultsList = ({title, results}) => {
 }
 const styles = StyleSheet.create({
     titleStyle: {
-        marginLeft:15,
-        fontSize:20,
-        fontWeight:'bold',
-        marginBottom:5
+        marginLeft: 15,
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 5
     },
     container: {
-        marginBottom:10
+        marginBottom: 10
     }
 })
 
