9b7b4d3597fb526158ea9da14c66a35cb27c9964
Kyle Phillips <khillips375@yahoo.com>
2019-11-26 17:02:07 -0500

    Used withNavigation helper to avoid having to pass down navigation prop through the SearchScreen

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
+import { withNavigation } from 'react-navigation';
 import ResultsDetail from '../components/ResultDetail';
 
 
@@ -40,4 +41,4 @@ const styles = StyleSheet.create({
     }
 })
 
-export default ResultsList
+export default withNavigation(ResultsList)
