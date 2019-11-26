0d2ca43e4ff557e73861e40b90e2224915c0531b
Kyle Phillips <khillips375@yahoo.com>
2019-11-26 16:34:36 -0500

    Added navigation props using destructuring

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -4,7 +4,7 @@ import SearchBar from '../components/SearchBar';
 import useResults from '../hooks/useResults';
 import ResultsList from '../components/ResultsList'
 
-const SearchScreen = () => {
+const SearchScreen = ({navigation}) => {
     const [term, setTerm] = useState('')
     const [searchApi, results, errorMessage] = useResults()
 
@@ -16,20 +16,24 @@ const SearchScreen = () => {
 
     console.log(results)
     return (
-        <View>
+        // This flex allow me to scroll from top to bottom
+        //  A lot of styling issues can be solved by adding flex 1 to the parent View
+        // Especially when it's content that is being cut off or expanding beyond the screen
+        //  As an alternative I can also use empty tags as parent element. ex: <> </>
+        <View style={{ flex: 1}}>
             <SearchBar
                 term={term}
                 onTermChange={setTerm}
                 onTermSubmit={() => searchApi(term)}
             />
             {errorMessage ? <Text>{errorMessage}</Text> : null}
-            <Text>We have found {results.length} results </Text>
-            
+            {/* <Text>We have found {results.length} results </Text> */}
+
             {/* Scrollview allows smaller devices to scroll vertically */}
             <ScrollView>
-                <ResultsList results={filterResultsByPrice('$')} title="Cost Effective" />
-                <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier" />
-                <ResultsList results={filterResultsByPrice('$$$')} title="Big Spender" />
+                <ResultsList navigation={navigation} results={filterResultsByPrice('$')} title="Cost Effective" />
+                <ResultsList navigation={navigation} results={filterResultsByPrice('$$')} title="Bit Pricier" />
+                <ResultsList navigation={navigation} results={filterResultsByPrice('$$$')} title="Big Spender" />
             </ScrollView>
         </View>
     )
